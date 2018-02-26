d12dfdafd0f068bc0dc5e2da00ab8eaeebbd09d1
dhorning42 <dhorning@purview.net>
2018-02-26 11:06:18 -0500

    MASTER doing tutorial

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Dusty
+                    About Dusty!
                 </div>
 
                 <div class="links">
